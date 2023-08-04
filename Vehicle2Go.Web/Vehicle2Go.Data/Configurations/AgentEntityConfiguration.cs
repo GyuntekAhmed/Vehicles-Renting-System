3bbb17924f585f5c3cd8a085a4117970a76474a6
gyuntekahmed <gyuntekahmed@abv.bg>
2023-08-05 00:08:44 +0300

    Add Seeding for user and agent.

---
+++ Vehicle2Go.Web/Vehicle2Go.Data/Configurations/AgentEntityConfiguration.cs
@@ -0,0 +1,29 @@
+﻿namespace Vehicle2Go.Data.Configurations
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+    using Models.Agent;
+
+    public class AgentEntityConfiguration : IEntityTypeConfiguration<Agent>
+    {
+        public void Configure(EntityTypeBuilder<Agent> builder)
+        {
+            // Uncomment before Seeding DB
+            //builder.HasData(this.GenerateAgent());
+        }
+
+        private Agent GenerateAgent()
+        {
+            Agent agent = new Agent
+            {
+                Id = Guid.Parse("6FC60999-8FC8-46B6-A131-897EDD45A5F0"),
+                PhoneNumber = "+359893794549",
+                Address = "Silistra",
+                UserId = Guid.Parse("389A42BB-6250-4E01-A7C6-29632BF524FA"),
+            };
+
+            return agent;
+        }
+    }
+}
--- Vehicle2Go.Web/Vehicle2Go.Data/Configurations/ApplicationEntityConfiguration.cs
+++ Vehicle2Go.Web/Vehicle2Go.Data/Configurations/ApplicationEntityConfiguration.cs
@@ -14,6 +14,28 @@
 
             builder.Property(u => u.LastName)
                 .HasDefaultValue("Testov");
+            // Uncomment before Seeding DB
+            //builder.HasData(this.GenerateApplicationUser());
+        }
+
+        private ApplicationUser GenerateApplicationUser()
+        {
+            ApplicationUser user = new ApplicationUser
+            {
+                Id = Guid.Parse("389A42BB-6250-4E01-A7C6-29632BF524FA"),
+                UserName = "gyuntekahmed@abv.bg",
+                NormalizedUserName = "GYUNTEKAHMED@ABV.BG",
+                Email = "gyuntekahmed@abv.bg",
+                NormalizedEmail = "GYUNTEKAHMED@ABV.BG",
+                PasswordHash = "AQAAAAEAACcQAAAAEK9pUrX4QwPX9IBiaBysvKvCrXeM36jvfu3Z+m78TRtLJ1ZhnTrZvedK/a12Zhlppw==",
+                SecurityStamp = "4AAZPELNS27DV5ZVG7ZQO5GPW3Y3JSEA",
+                ConcurrencyStamp = "9d36e3b1-b876-4b86-b708-d757166579ad",
+                PhoneNumber = null,
+                FirstName = "Test",
+                LastName = "Testov",
+            };
+
+            return user;
         }
     }
 }
